43782601906f82418eec60d5a38092b3172090e7
Colin Thomas <cthom187@uncc.edu>
2019-03-31 17:37:55 +0000

    generate and run Subjects migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_154952) do
+ActiveRecord::Schema.define(version: 2019_03_31_173648) do
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "subject"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
