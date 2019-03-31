652a8e87f6d8371f3ac7183243b2659cf1074629
Colin Thomas <cthom187@uncc.edu>
2019-03-31 17:35:29 +0000

    created show, new, and create views

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,4 +1,19 @@
 class SubjectsController < ApplicationController
   def index
+    
   end
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+    render plain: params[:subject].inspect
+  end
+  
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'subjects/index'
+  get 'subjects/show'
   root 'subjects#index'
   devise_for :users
+  resources :subjects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
