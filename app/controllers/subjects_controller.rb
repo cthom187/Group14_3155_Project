9ceaa35a499712754145c36222ac49f371cc217a
Colin Thomas <cthom187@uncc.edu>
2019-03-31 18:16:31 +0000

    added list of subjects for Subjects/index view

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,19 +1,28 @@
 class SubjectsController < ApplicationController
   def index
-    
+    @subjects = Subject.all
   end
   
   def show
+
   end
   
   def new
   end
   
   def create
-    render plain: params[:subject].inspect
+    @subject = Subject.new(params[subject_params])
+    
+    @subject.save
+    redirect_to @subject
   end
   
   def destroy
   end
 
 end
+
+private
+  def subject_params
+    params.require(:subject).permit(:subject)
+  end 
